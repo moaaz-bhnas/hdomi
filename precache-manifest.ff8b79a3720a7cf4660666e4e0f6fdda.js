0f6b7ec4873bf63f4a175235ed612f4b456cf7d2
moaaz-bhnas <moaaz_bs@yahoo.com>
2020-04-17 06:06:44 +0200

    Updates

---
+++ precache-manifest.ff8b79a3720a7cf4660666e4e0f6fdda.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d30391824acb5a09609b9834198a78c",
+    "url": "/hdomi/index.html"
+  },
+  {
+    "revision": "7996a5f01744cdb7f074",
+    "url": "/hdomi/static/js/2.fd338f0f.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/hdomi/static/js/2.fd338f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f8e77a72eac6e35912",
+    "url": "/hdomi/static/js/main.930b42c7.chunk.js"
+  },
+  {
+    "revision": "0c6b7ea2abe6075eacba",
+    "url": "/hdomi/static/js/runtime-main.1644c9fa.js"
+  },
+  {
+    "revision": "804c9e2bd12cc3ee47f3d54a833958e3",
+    "url": "/hdomi/static/media/banner.804c9e2b.jpg"
+  },
+  {
+    "revision": "e5f3ec247f6ab6fb64af38e2bf4cc163",
+    "url": "/hdomi/static/media/down-arrow.e5f3ec24.svg"
+  },
+  {
+    "revision": "0f0a056e8d5201f34189fc50f0566f97",
+    "url": "/hdomi/static/media/heart.0f0a056e.svg"
+  },
+  {
+    "revision": "de7a8f868f5f6b39bb01380803bf0384",
+    "url": "/hdomi/static/media/kids1.de7a8f86.jpg"
+  },
+  {
+    "revision": "02dd19c144774feda1e51bb423b4d0eb",
+    "url": "/hdomi/static/media/men2.02dd19c1.jpg"
+  },
+  {
+    "revision": "0ab7e52e6bbfd4e2c534296c6f3a7903",
+    "url": "/hdomi/static/media/menu.0ab7e52e.svg"
+  },
+  {
+    "revision": "a33906a14a1b8ad985cf304d7b3b6e67",
+    "url": "/hdomi/static/media/orders.a33906a1.svg"
+  },
+  {
+    "revision": "b2feb1c702b9b4c46343cfc222770c0d",
+    "url": "/hdomi/static/media/search.b2feb1c7.svg"
+  },
+  {
+    "revision": "fe52ed05e8e5d0d23a97687338d8eef7",
+    "url": "/hdomi/static/media/texture.fe52ed05.png"
+  },
+  {
+    "revision": "a78ba0985e2e79f70c3c31659b8ad493",
+    "url": "/hdomi/static/media/women.a78ba098.jpeg"
+  }
+]);
