7ab39326bfd396f30a3e4982392054ea46a0cafc
moaaz-bhnas <moaaz_bs@yahoo.com>
2020-04-23 21:22:28 +0200

    added nested pages od seller

--- src/pages/seller/index.js
+++ src/pages/seller/index.js
@@ -1,6 +1,8 @@
 import React, { memo } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
+import Products from './pages/products';
+import SponsoredProducts from './pages/sponsoredProducts';
 
 const Seller = ({ match: { url } }) => {
   console.log(url);
@@ -10,6 +12,14 @@ const Seller = ({ match: { url } }) => {
         exact
         path={`${url}/`}
         component={Dashboard}
+      />
+      <Route 
+        path={`${url}/products`}
+        component={Products}
+      />
+      <Route 
+        path={`${url}/sponsored-products`}
+        component={SponsoredProducts}
       />
     </Switch>
   );
---
+++ src/pages/seller/pages/products.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+const Products = () => {
+  return (
+    <>Products</>
+  );
+}
+
+export default memo(Products);
